a9021ac96c08aa403e9af09f6e32a50d838d47fa
CodingManoj <CodingManoj@gmail.com>
2024-09-10 20:59:30 -0400

    AutoPush

--- variables.tf
+++ variables.tf
@@ -2,7 +2,7 @@
 variable "tools" {
   default = {
     vault = {
-      name          = "vault"
+      name          = "hashicorp-vault"
       instance_type = "t3.small"
       port_no       = 8200
     }
