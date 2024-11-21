952fc18cb6f7aa3b2bfba6aae4207b1b4a1f5c76
CodingManoj <CodingManoj@gmail.com>
2024-11-20 20:26:50 -0500

    GitHub Actions: CICD

--- module/route53.tf
+++ module/route53.tf
@@ -1,8 +1,18 @@
-# Creates DNS Record
+# Creates private DNS Record
 resource "aws_route53_record" "main" {
   zone_id = data.aws_route53_zone.main.zone_id
   name    = "${var.name}.expense.internal"
   type    = "A"
   ttl     = 10
   records = [aws_instance.main.private_ip]
+}
+
+# Creates public dns record
+
+resource "aws_route53_record" "public" {
+  zone_id = "Z0520454XBSCKDOCPTCY"
+  name    = "${var.name}.cloudapps.today"
+  type    = "A"
+  ttl     = 10
+  records = [aws_instance.main.public_ip]
 }
