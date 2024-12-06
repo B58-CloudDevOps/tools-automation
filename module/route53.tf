48ac4b53aba7a3f53634cb7b01b56dfd1f366a65
CodingManoj <CodingManoj@gmail.com>
2024-12-05 19:38:13 -0500

    Expense Infra

--- module/route53.tf
+++ module/route53.tf
@@ -1,18 +1,17 @@
 # Creates private DNS Record
 resource "aws_route53_record" "main" {
   zone_id = data.aws_route53_zone.main.zone_id
-  name    = "${var.name}.expense.internal"
+  name    = "${var.name}.roboshop.internal"
   type    = "A"
   ttl     = 10
   records = [aws_instance.main.private_ip]
 }
 
 # Creates public dns record
-
-# resource "aws_route53_record" "public" {
-#   zone_id = "Z0520454XBSCKDOCPTCY"
-#   name    = "${var.name}.cloudapps.today"
-#   type    = "A"
-#   ttl     = 10
-#   records = [aws_instance.main.public_ip]
-# }
+resource "aws_route53_record" "public" {
+  zone_id = "Z0520454XBSCKDOCPTCY"
+  name    = "${var.name}.cloudapps.today"
+  type    = "A"
+  ttl     = 10
+  records = [aws_instance.main.public_ip]
+}
--- variables.tf
+++ variables.tf
@@ -8,52 +8,5 @@ variable "tools" {
         vault = 8200
       }
     }
-    # prometheus = {
-    #   name          = "prometheus"
-    #   instance_type = "t3.small"
-
-    #   port_no = {
-    #     prometheus = 9090
-    #   }
-    # }
-
-    # grafana = {
-    #   name          = "grafana"
-    #   instance_type = "t3.small"
-
-    #   port_no = {
-    #     grafana = 3000
-    #   }
-    # }
-
-    elk = {
-      name          = "elk"
-      instance_type = "r7a.large"
-
-      port_no = {
-        kibana   = 80
-        logstash = 5044
-      }
-    }
-
-    ci-runner = {
-      name          = "ci-runner"
-      instance_type = "t3.micro"
-
-      port_no = {
-        # kibana   = 80
-        # logstash = 5044
-      }
-    }
-
-    sonar = {
-      name          = "sonar"
-      instance_type = "r7a.large"
-
-      port_no = {
-        sonar = 9000
-      }
-    }
-
   }
 }
