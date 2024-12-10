820b0d6ff66cc284038fd76c8467828b87cb9355
CodingManoj <CodingManoj@gmail.com>
2024-12-09 20:42:11 -0500

    Dry Code

--- setup-tools.sh
+++ setup-tools.sh
@@ -1,4 +1,4 @@
 # ansible-playbook -i ${tool_name}.expense.internal, -e ansible_user=ec2-user -e ansible_password=${ssh_pwd} -e tool_name=${tool_name}  setup-tools.yml
 
 git pull
-ansible-playbook -i ${2}.expense.internal, -e ansible_user=ec2-user -e ansible_password=${1} -e tool_name=${2} -e vault_token=${3} setup-tools.yml
+ansible-playbook -i ${2}.roboshop.internal, -e ansible_user=ec2-user -e ansible_password=${1} -e tool_name=${2} -e vault_token=${3} setup-tools.yml
